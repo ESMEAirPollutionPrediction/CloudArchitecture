2e822733064c7109a8f6db060e676efb8e8e08cb
Robin Lotode <lotode.rob@gmail.com>
2024-02-24 20:41:25 +0100

    update website

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import folium
 
 from flask import Flask, render_template
 
@@ -33,11 +34,35 @@ def create_app(test_config=None):
     def index():
         return render_template('index.html')
 
+    @app.route('/map')
+    def map():
+        m = folium.Map(location=[47, 2.2137],
+                       zoom_start=6,
+                       width=800,
+                       height=600,)
+        m.get_root().render()
+        header = m.get_root().header.render()
+        body_html = m.get_root().html.render()
+        script = m.get_root().script.render()
+        header = header.replace("""
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"/>"""
+                                , "")
+        print(header)
+        return render_template('map.html',
+            header=header,
+            body_html=body_html,
+            script=script,)
+
     @app.route('/log')
     def log():
         log_path = "/var/log/Flask.err.log"
         with open(log_path) as f:
             content = f.readlines()
         return render_template('log.html', content=content)
+    
+    @app.route('/test')
+    def test():
+        return render_template('test.html')
 
     return app
